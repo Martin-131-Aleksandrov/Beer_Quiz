e37b76f907e5b5f706efa305fa98c12456c3d57c
Martin-131-Aleksandrov <martin131aleksandrov@yahoo.com>
2023-03-06 01:49:19 +0200

    first commit

---
+++ beer_quiz.py
@@ -0,0 +1,136 @@
+beer_styles_info = {
+    "Beers with pale color": {
+        "low alcoholic beers (<4% ABV)": {
+            "Bitter, but not aromatic": ["Session India Pale Ale", "Ordinary Bitter", "German-Style Leichtbier"],
+            "Aromatic,but not bitter": ["Session Beer", "German-Style Leichtes Weizen", "American-Style Fruit Beer",
+                                        "Fruit Wheat Beer"],
+            "Balanced - aromatic and bitter": ["Session India Pale Ale", "Belgian-Style Session Ale",
+                                               "Scottish-Style Light Ale", "English-Style Summer Ale",
+                                               "Grodziskie - Smoked ale", "American-Style Amber Light Lager",
+                                               "Specialty Honey Beer"],
+            "Sour": ["Session Beer", "Berliner-Style Weisse", "American-Style Sour Ale"]
+        },
+        "common beers(4-7% ABV)": {
+            "Bitter, but not aromatic": ["British-Style India Pale Ale", "German-Style Koelsch",
+                                         "German-Style Altbier", "International-Style Pale Ale",
+                                         "German-Style Pilsner", "Munich-Style Helles", "Vienna-Style Lager",
+                                         "American-Style Cream Ale", "California Common Beer", "German-Style Maerzen",
+                                         "Bamberg-Style Maerzen Rauchbier", "American-Style India Pale Lager"],
+            "Aromatic,but not bitter": ["American-Style Fruit Beer", "Herb and Spice Beer", "Blonde Ale",
+                                        "Juicy or Hazy Pale Ale", "Belgian-Style Witbier", "American-Style Lager",
+                                        "New England India Pale Lager"],
+            "Balanced - aromatic and bitter": ["American-Style Wheat Beer", "Fruit Wheat Beer", "Pumpkin/Squash Beer",
+                                               "Chili Pepper Beer", "Specialty Honey Beer",
+                                               "South German-Style Hefeweizen", "South German-Style Kristal Weizen",
+                                               "German-Style Rye Ale", "Bamberg-Style Weiss Rauchbier",
+                                               "American-Style Ice Lager", "American-Style Pilsener",
+                                               "International-Style Pilsener", "American-Style Strong Pale Ale",
+                                               "Classic French & Belgian-Style Saison", "German-Style Oktoberfest",
+                                               "Dortmunder/European-Style Export"],
+            "Sour": ["Leipzig-Style Gose", "American-Style Sour Ale", "Belgian-Style Lambic",
+                     "Traditional Belgian-Style Gueuze", "Belgian-Style Fruit Lambic"]
+        },
+        "high alcoholic beers(7-10% ABV)": {
+            "Bitter, but not aromatic": ["Strong Ale", "British-Style Barley Wine Ale", "Double India Pale Ale",
+                                         "Juicy or Hazy Imperial or Double India Pale Ale"],
+            "Aromatic,but not bitter": ["American-Style Fruit Beer", "Fruit Wheat Beer",
+                                        "Juicy or Hazy  New England Imperial or Double India Pale Ale"],
+            "Balanced - aromatic and bitter": ["Specialty Honey Beer", "South German-Style Weizenbock",
+                                               "Belgian-Style Strong Blonde Ale", "British-Style Barley Wine Ale"],
+            "Sour": ["American-Style Sour Ale"]
+        },
+        "strong beers(10%< ABV)": {
+            "Bitter, but not aromatic": ["British-Style Barley Wine Ale"],
+            "Aromatic,but not bitter": ["American-Style Fruit Beer", "Fruit Wheat Beer"],
+            "Balanced - aromatic and bitter": ["Specialty Honey Beer", "British-Style Barley Wine Ale"],
+            "Sour": ["American-Style Sour Ale"]
+        }
+    },
+    "Beers with red color":  {
+        "low alcoholic beers (<4% ABV)": {
+            "Bitter, but not aromatic": ["Session Beer"],
+            "Aromatic,but not bitter": ["Belgian-Style Table Beer"],
+            "Balanced - aromatic and bitter": ["Belgian-Style Table Beer"],
+            "Sour": ["Session Beer", "American-Style Sour Ale"]
+        },
+        "common beers(4-7% ABV)": {
+            "Bitter, but not aromatic": ["Irish-Style Red Ale", "American-Style Amber/Red Ale"],
+            "Aromatic,but not bitter": ["American-Style Amber Lager"],
+            "Balanced - aromatic and bitter": ["Irish-Style Red Ale", "Belgian-Style Flanders Oud Bruin"],
+            "Sour": ["Belgian-Style Flanders Oud Bruin or Oud Red Ale", "American-Style Sour Ale"]
+        },
+        "high alcoholic beers(7-10% ABV)": {
+            "Bitter, but not aromatic": ["Imperial Red Ale"],
+            "Aromatic,but not bitter": ["Double Hoppy Red Ale", "Scotch Ale"],
+            "Balanced - aromatic and bitter": ["Old Ale"],
+            "Sour": ["American-Style Sour Ale"]
+        },
+        "strong beers(10%< ABV)": {
+            "Bitter, but not aromatic": ["Imperial Red Ale"],
+            "Aromatic,but not bitter": ["Session Beer"],
+            "Balanced - aromatic and bitter": ["Imperial Red Ale"],
+            "Sour": ["American-Style Sour Ale"]
+        }
+    },
+    "Beers with dark color":  {
+        "low alcoholic beers (<4% ABV)": {
+            "Bitter, but not aromatic": ["Session Beer", "Scottish-Style Heavy Ale", "Cream Stout", ],
+            "Aromatic,but not bitter": ["Session Beer"],
+            "Balanced - aromatic and bitter": ["Session Beer", "Belgian-Style Table Beer",
+                                               "English-Style Dark Mild Ale", "Specialty Honey Beer"],
+            "Sour": ["Sour Session Beer"]
+        },
+        "common beers(4-7% ABV)": {
+            "Bitter, but not aromatic": ["Oatmeal Stout", "American-Style Brown Ale", "Munich-Style Dunkel",
+                                         "European-Style Dark Lager", "German-Style Schwarzbier", "Robust Porter",
+                                         "Export-Style Stout", "Smoke Porter"],
+            "Aromatic,but not bitter": ["Herb and Spice Beer"],
+            "Balanced - aromatic and bitter": ["English-Style Brown Ale", "Brown Porter", "Herb and Spice Beer",
+                                               "Specialty Honey Beer", "South German-Style Dunkel Weizen",
+                                               "American-Style Dark Lager", "American-Style Stout",
+                                               "American-Style Black Ale", "Bamberg-Style Bock Rauchbier",
+                                               "Traditional German-Style Bock", "German-Style Doppelbock"],
+            "Sour": ["Sour Session Beer"]
+        },
+        "high alcoholic beers(7-10% ABV)": {
+            "Bitter, but not aromatic": ["Smoke Porter", "Strong Ale", "British-Style Imperial Stout",
+                                         "American-Style Imperial Porter", "American-Style Imperial Stout",
+                                         "Baltic-Style Porter"],
+            "Aromatic,but not bitter": ["Herb and Spice Beer", "Adambier"],
+            "Balanced - aromatic and bitter": ["Herb and Spice Beer", "Specialty Honey Beer", "American-Style Stout",
+                                               "American-Style Black Ale", "Belgian-Style Strong Dark Ale",
+                                               "Belgian-Style Dubbel", "Belgian-Style Tripel", "Baltic-Style Porter"],
+            "Sour": ["American-Style Fruit Sour Beer"]
+        },
+        "strong beers(10%< ABV)": {
+            "Bitter, but not aromatic": ["American-Style Barley Wine Ale", "Belgian-Style Quadrupel"],
+            "Aromatic,but not bitter": ["German-Style Eisbock"],
+            "Balanced - aromatic and bitter": ["Specialty Honey Beer", "American-Style Barley Wine Ale",
+                                               "German-Style Eisbock", "Belgian-Style Quadrupel"],
+            "Sour": ["American-Style Fruit Sour Beer"]
+        }
+    }
+}
+
+
+if __name__ == "__main__":
+    current_data = beer_styles_info
+
+    while type(current_data) == dict:
+        print("What kind of beer do you prefer?")
+        counter = 64
+        answers = {}
+        for option in current_data:
+            counter += 1
+            print(f"{chr(counter)} : {option}", end=' | ')
+            answers[chr(counter)] = option
+        print()
+        userinput = input().upper()
+        if userinput in answers:
+            current_data = current_data[answers[userinput]]
+        else:
+            print("Choose one of the provided options! Try again!")
+    if type(current_data) == list:
+        print(f"Great, we found {len(current_data)} beer styles suitable for your taste:")
+        for beer in current_data:
+            print(beer)
